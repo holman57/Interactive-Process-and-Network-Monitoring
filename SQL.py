5e30ceaf932e18ccfc33c69d66fc6a149fcdd65e
Luke Holman <holman57@gmail.com>
2020-11-12 22:52:42 -0600

    Access database at Drop_Points

--- SQL.py
+++ SQL.py
@@ -1,16 +1,16 @@
 import mysql.connector
+from secrete.Secretes import Dorp_Points
 
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    password="yourpassword",
-    database="mydatabase"
+    host=Dorp_Points.host,
+    user=Dorp_Points.user,
+    port=Dorp_Points.port,
+    password=Dorp_Points.password,
+    database=Dorp_Points.database
 )
 
 mycursor = mydb.cursor()
-
-mycursor.execute('SELECT * FROM customers')
-
+mycursor.execute('SELECT * FROM Nodes')
 myresult = mycursor.fetchall()
 
 for x in myresult:
